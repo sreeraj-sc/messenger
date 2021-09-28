42c2f459d2c64a7b151ea4c8d6509bbd9ede3b1a
Sreeraj S Chandran <sreeraj.s.c.vfc@gmail.com>
2021-09-28 11:48:23 +0530

    done but error compilation failed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { ChatEngine } from 'react-chat-engine';
 
+import LoginForm from './components/ChatFeed';
 import ChatFeed from './components/ChatFeed';
 
 import './App.css';
 
 const App = () => {
+    if(!localStorage.getItem('username')) return <LoginForm />
+
     return (
         <ChatEngine 
             height="100vh"
             projectID="edd67718-5fdb-43c0-b023-2055576a9773"
-            userName="sreeraj"
-            userSecret="121121"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
             renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
         />
     )
